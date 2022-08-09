a64ccefa06b3ad17465baaaa4d31e44ca3d0a384
Eddy Löwen <mail@eddyloewen.com>
2022-08-09 12:39:32 +0200

    moves cypress to /tests directory

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'cypress';
-import ContactFactory from "./database/factories/ContactFactory.js";
+import ContactFactory from './database/factories/ContactFactory.js';
 import OrganizationFactory from './database/factories/OrganizationFactory.js';
-import UserFactory from "./database/factories/UserFactory.js";
+import UserFactory from './database/factories/UserFactory.js';
 
 const factories = {
 	contact: ContactFactory,
 	organisation: OrganizationFactory,
 	user: UserFactory,
-}
+};
 
 //module.exports = defineConfig({
 export default defineConfig({
+	downloadsFolder: 'tests/cypress/downloads',
+	fixturesFolder: 'tests/cypress/fixtures',
+	screenshotsFolder: 'tests/cypress/screenshots',
+	videosFolder: 'tests/cypress/videos',
+
 	chromeWebSecurity: false,
 	e2e: {
+		supportFile: 'tests/cypress/support/e2e.js',
+		specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 			on('task', {
